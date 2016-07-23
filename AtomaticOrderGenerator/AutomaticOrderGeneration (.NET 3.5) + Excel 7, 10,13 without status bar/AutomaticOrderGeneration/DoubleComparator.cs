31f659c371c26b6d88938beb2e9ee6cce4e32abc
darya_kolyadko <skvoznak1996@gmail.com>
2016-07-23 03:16:52 +0300

    Add DoubleComparator (problems with new currency comparasion)

---
+++ AtomaticOrderGenerator/AutomaticOrderGeneration (.NET 3.5) + Excel 7, 10,13 without status bar/AutomaticOrderGeneration/DoubleComparator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomaticOrderGeneration
+{
+    class DoubleComparator: IComparer<Double>
+    {
+        private static double permissableError = Math.Pow(10, -10);
+        public int Compare(double x, double y)
+        {
+            double diff = Math.Abs(x - y);
+
+            if (diff < permissableError | diff.Equals(permissableError))
+            {
+                return 0;
+            }
+
+            if (x - y > permissableError)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+}
